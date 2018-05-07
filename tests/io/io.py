f676ed654b928e6098944a6d8420039a8092781e
Luab <b.maksudov@innopolis.ru>
2018-05-07 18:08:33 +0300

    Fixed io tests

--- tests/io/io.py
+++ tests/io/io.py
@@ -9,12 +9,6 @@ from src.io.io import Io
 import unittest
 import src.config as config
 
-<<<<<<< HEAD
-=======
-
-class TestIO(unittest.TestCase):
->>>>>>> b4c9c5d265fa63d6d0f5701f2bfc82ff685c4f61
-
 class TestIO(unittest.TestCase):
     def test_store_io(self):
         io = Io()
